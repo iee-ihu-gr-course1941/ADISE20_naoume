854d35e20d8952b86c1112bbfac7154122c24fe4
ntros12 <ntrosoxonntr@windowslive.com>
2021-01-03 18:22:00 +0200

    Add files via upload

---
+++ Move2.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class Move2 implements Move{
+
+    private $numberofpoints;
+
+    public function getNumberOfPoints(){
+
+        return $this->numberofpoints;
+
+    }
+
+    public function setNumberOfPoints($numberofpoints){
+
+        $this->numberofpoints = $numberofpoints;
+    }
+
+
+}
+
+?>
---
+++ score4.sql
@@ -0,0 +1,220 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 14, 2020 at 08:43 PM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `score4`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `clean_board` ()  BEGIN
+REPLACE INTO board SELECT * FROM board_empty;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `move_piece` (`x1` TINYINT, `y1` TINYINT)  BEGIN
+DECLARE p_color CHAR;
+UPDATE board
+SET piece_color=p_color
+WHERE X=x1 AND Y=y1;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `board`
+--
+
+CREATE TABLE `board` (
+  `x` tinyint(1) NOT NULL,
+  `y` tinyint(1) NOT NULL,
+  `piece_color` enum('R','B') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `board`
+--
+
+INSERT INTO `board` (`x`, `y`, `piece_color`) VALUES
+(1, 1, NULL),
+(1, 2, NULL),
+(1, 3, NULL),
+(1, 4, NULL),
+(1, 5, NULL),
+(1, 6, NULL),
+(2, 1, NULL),
+(2, 2, NULL),
+(2, 3, NULL),
+(2, 4, NULL),
+(2, 5, NULL),
+(2, 6, NULL),
+(3, 1, NULL),
+(3, 2, NULL),
+(3, 3, NULL),
+(3, 4, NULL),
+(3, 5, NULL),
+(3, 6, NULL),
+(4, 1, NULL),
+(4, 2, NULL),
+(4, 3, NULL),
+(4, 4, NULL),
+(4, 5, NULL),
+(4, 6, NULL),
+(5, 1, NULL),
+(5, 2, NULL),
+(5, 3, NULL),
+(5, 4, NULL),
+(5, 5, NULL),
+(5, 6, NULL),
+(6, 1, NULL),
+(6, 2, NULL),
+(6, 3, NULL),
+(6, 4, NULL),
+(6, 5, NULL),
+(6, 6, NULL),
+(7, 1, NULL),
+(7, 2, NULL),
+(7, 3, NULL),
+(7, 4, NULL),
+(7, 5, NULL),
+(7, 6, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `board_empty`
+--
+
+CREATE TABLE `board_empty` (
+  `x` tinyint(1) NOT NULL,
+  `y` tinyint(1) NOT NULL,
+  `piece_color` enum('R','B') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `board_empty`
+--
+
+INSERT INTO `board_empty` (`x`, `y`, `piece_color`) VALUES
+(1, 1, NULL),
+(1, 2, NULL),
+(1, 3, NULL),
+(1, 4, NULL),
+(1, 5, NULL),
+(1, 6, NULL),
+(2, 1, NULL),
+(2, 2, NULL),
+(2, 3, NULL),
+(2, 4, NULL),
+(2, 5, NULL),
+(2, 6, NULL),
+(3, 1, NULL),
+(3, 2, NULL),
+(3, 3, NULL),
+(3, 4, NULL),
+(3, 5, NULL),
+(3, 6, NULL),
+(4, 1, NULL),
+(4, 2, NULL),
+(4, 3, NULL),
+(4, 4, NULL),
+(4, 5, NULL),
+(4, 6, NULL),
+(5, 1, NULL),
+(5, 2, NULL),
+(5, 3, NULL),
+(5, 4, NULL),
+(5, 5, NULL),
+(5, 6, NULL),
+(6, 1, NULL),
+(6, 2, NULL),
+(6, 3, NULL),
+(6, 4, NULL),
+(6, 5, NULL),
+(6, 6, NULL),
+(7, 1, NULL),
+(7, 2, NULL),
+(7, 3, NULL),
+(7, 4, NULL),
+(7, 5, NULL),
+(7, 6, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `game_status`
+--
+
+CREATE TABLE `game_status` (
+  `status` enum('not active','initialized','started','ended','aborted') NOT NULL DEFAULT 'not active',
+  `p_turn` enum('R','B') DEFAULT NULL,
+  `result` enum('R','B','D') DEFAULT NULL,
+  `last_change` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Triggers `game_status`
+--
+DELIMITER $$
+CREATE TRIGGER `game_status_update` BEFORE UPDATE ON `game_status` FOR EACH ROW BEGIN
+SET NEW.last_change = NOW();
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `players`
+--
+
+CREATE TABLE `players` (
+  `username` varchar(20) DEFAULT NULL,
+  `piece_color` enum('R','B') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `board`
+--
+ALTER TABLE `board`
+  ADD PRIMARY KEY (`x`,`y`);
+
+--
+-- Indexes for table `board_empty`
+--
+ALTER TABLE `board_empty`
+  ADD PRIMARY KEY (`x`,`y`);
+
+--
+-- Indexes for table `players`
+--
+ALTER TABLE `players`
+  ADD PRIMARY KEY (`piece_color`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
